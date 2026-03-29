ad28e8e92471249bc504d401df38f55ee09961e3
valisoa01 <valisoatolotriniaina@gmail.com>
2026-03-29 14:43:07 +0300

    feat:add DishController

--- src/main/java/com/example/demo/controller/DishController.java
+++ src/main/java/com/example/demo/controller/DishController.java
@@ -1,4 +1,23 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.DishEntity;
+import com.example.demo.service.DishService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/dishes")
 public class DishController {
+    private final DishService dishService;
+
+    public DishController(DishService dishService) {
+        this.dishService = dishService;
+    }
+    @GetMapping
+    public List<DishEntity> findAllDish() {
+        return dishService.findAllDish();
+    }
 }
